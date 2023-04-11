331876b14bcc507e6aadedef0a8c47d9a9f0eaf2
Mark Alfred Macapagal <mark.alfred072915@gmail.com>
2023-04-11 21:54:14 +0800

    Add city and province seeder

---
+++ database/migrations/2022_09_01_085457_create_cities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 191)
+                ->index()
+                ->nullable();
+            $table->unsignedBigInteger('reg_id')->nullable();
+            $table->foreignId('province_id')
+                ->nullable()
+                ->constrained('provinces')
+                ->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
