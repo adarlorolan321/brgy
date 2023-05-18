ab1eabde1adaafab687042414aeada63f170ffef
DESKTOP-TK21QG9\melodybolinas <bolinasmelody2@gmail.com>
2023-05-18 21:12:01 +0800

    run build

---
+++ public/build/assets/Welcome-0b74f934.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AdminLayout-30e0d3c5.js";import{_ as e,o as t,c}from"./app-007b4b42.js";const n={layout:o,mounted:function(){}};function r(a,s){return t(),c("h1",null,"Dashboard")}const f=e(n,[["render",r]]);export{f as default};
